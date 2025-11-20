7d96fe3648bc5d2191cf81c0d0dc59d12b428449
JoshTabsDEv <tabura.joshua@cec.edu.ph>
2025-11-20 14:34:39 +0800

    firsts commit

--- server/server.ts
+++ server/server.ts
@@ -2,16 +2,29 @@ import express from "express";
 import cors from "cors";
 import mysql from "mysql2/promise";
 import { OAuth2Client } from "google-auth-library";
+import dotenv from 'dotenv';
+
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: '.env' });
+}
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// const db = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "typescript1",
+// });
+
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "typescript1",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
 });
 
 const client = new OAuth2Client("759282268919-cfu3n7h5ao3e7v5f2dpsaoj2r7njj8t0.apps.googleusercontent.com");
